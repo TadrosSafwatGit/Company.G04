623089b466db11d74bf0c7b6e4549a6ba303e263
tadd <tadd@DESKTOP-T0VD92O>
2025-03-09 00:07:57 +0200

    Add project files.

---
+++ MVC.3.PL/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVC._3.PL.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
