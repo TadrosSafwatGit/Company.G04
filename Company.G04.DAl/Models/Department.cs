1a2b74213cff6ae523f1f12dc65518e3dd2009cc
TadrosSafwatGit <https://github.com/TadrosSafwatGit>
2025-03-09 02:19:17 +0200

    DAL impl##

---
+++ Company.G04.DAl/Models/Department.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+
+namespace Company.G04.DAl.Models
+{
+    internal class Department
+    {
+        public int Id { get; set; }
+
+        public string Code { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime CreateAt { get; set; }
+
+    }
+}
